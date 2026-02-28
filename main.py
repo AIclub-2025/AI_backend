b5ef964641f59cc93df81fdee342e90689cb1f4d
imimbert <2080035494@qq.com>
2026-02-28 21:41:44 +0800

    fix cors

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from api import praise_report, campus_news
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import os
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins = ["*"],
-    allow_credentials = True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+# 处理跨域请求和 Private Network Access
+@app.middleware("http")
+async def cors_middleware(request: Request, call_next):
+    # 获取请求的 Origin，如果没有则默认为 *
+    origin = request.headers.get("Origin", "*")
+    
+    # 处理预检请求 (OPTIONS)
+    if request.method == "OPTIONS":
+        response = Response()
+        response.headers["Access-Control-Allow-Origin"] = origin
+        response.headers["Access-Control-Allow-Methods"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = "*"
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        response.headers["Access-Control-Max-Age"] = "3600"
+        
+        # 处理 Private Network Access (PNA)
+        if request.headers.get("Access-Control-Request-Private-Network") == "true":
+            response.headers["Access-Control-Allow-Private-Network"] = "true"
+            
+        return response
+
+    # 处理正常请求
+    response = await call_next(request)
+    
+    # 添加 CORS 头
+    response.headers["Access-Control-Allow-Origin"] = origin
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    
+    # 处理 Private Network Access (PNA)
+    if request.headers.get("Access-Control-Request-Private-Network") == "true":
+        response.headers["Access-Control-Allow-Private-Network"] = "true"
+        
+    return response
 
 @app.get("/")
 async def root():
